149fb2699c07827881ac6cc2ee9b9feb2b6e32c6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-06-30 12:00:52 -0300

    Controller Weather

---
+++ Website/Dados/Web.Data/Controllers/WeatherController.cs
@@ -0,0 +1,26 @@
+﻿namespace Web.Data.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using Web.Data.DAO;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherController : ControllerBase
+{
+    private readonly DB db;
+    private readonly ILogger log;
+
+    public WeatherController(DB db, ILogger log)
+    {
+        this.db = db;
+        this.log = log;
+    }
+
+    [HttpGet]
+    public IActionResult Obter()
+    {
+        return Ok(db.ObterWeatherProximasHoras());
+    }
+
+}
