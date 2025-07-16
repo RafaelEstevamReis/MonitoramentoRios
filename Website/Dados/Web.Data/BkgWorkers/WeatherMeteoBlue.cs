cface1dfcc95e3a596c3d7998cbaab681b1cdfb6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-07-16 01:25:58 -0300

    Update WeatherMeteoBlue.cs

--- Website/Dados/Web.Data/BkgWorkers/WeatherMeteoBlue.cs
+++ Website/Dados/Web.Data/BkgWorkers/WeatherMeteoBlue.cs
@@ -39,7 +39,7 @@ public class WeatherMeteoBlue : IHostedService, IDisposable
         }
 
         logger.Information("[WeatherMeteoBlue] Iniciando serviço de meteorologia MeteoBlue...");
-        _timer = new Timer(executaVerificacaoAsync, null, TimeSpan.FromMinutes(1), TimeSpan.FromHours(13));
+        _timer = new Timer(executaVerificacaoAsync, null, TimeSpan.FromHours(1), TimeSpan.FromHours(13));
 
         await Task.CompletedTask;
     }
