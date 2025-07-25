af92b2eeb9bb918ac59e3fa77be574eefae2bc53
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-07-25 10:50:50 -0300

    Adiciona calibração para ignorar a raíz

---
+++ Website/Dados/Web.Data/CalibracaoDigital/RSRL-AR01/CD20250725_WL.cs
@@ -0,0 +1,33 @@
+﻿namespace Web.Data.CalibracaoDigital.RSRL_AR01;
+
+using System;
+using System.Text.Json.Nodes;
+using Web.Data.Controllers;
+
+public class CD20250725_WL : ICalibracaoDigital
+{
+    public string Estacao => "CF98FCFA7E9EE7C1";
+
+    public DateTime ValidadeInicioUTC => new DateTime(2025, 07, 25, 0, 0, 0, DateTimeKind.Utc);
+    public DateTime ValidadeFimUTC => new DateTime(2025, 08, 25, 0, 0, 0, DateTimeKind.Utc); // Em 04/07 foi corrigido o envio
+
+    public bool ProcessaDados(UpController.UploadData d, JsonNode? r)
+    {
+        if (!d.NivelRio_RAW.HasValue) return false;
+
+        if(d.NivelRio_RAW > 5.4M)
+        {
+            d.NivelRio = null; // Além do alcance do sensor
+            return true;
+        }
+
+        // Pegou na Raíz
+        if (d.NivelRio_RAW > 1.5M && d.NivelRio_RAW < 2.1M)
+        {
+            d.NivelRio = null; // Pegou a raíz
+            return true;
+        }
+
+        return false; // Não mexeu
+    }
+}
