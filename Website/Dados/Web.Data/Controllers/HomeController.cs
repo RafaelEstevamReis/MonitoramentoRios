81c2ce8cbe8401fafacef42f6bf9cdfa286ab9b8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-08-14 18:23:58 -0300

    Adicionar detalhes de versão

--- Website/Dados/Web.Data/Controllers/HomeController.cs
+++ Website/Dados/Web.Data/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿namespace Web.Data.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System;
+using System.Diagnostics;
 
 [ApiController]
 public class HomeController : ControllerBase
@@ -28,5 +31,38 @@ public class HomeController : ControllerBase
     public record VersionInfo
     {
         public string Revision { get; set; } = string.Empty;
+        public string Enviroment { get; set; } = Environment.OSVersion.Platform.ToString();
+        public string Server { get; set; } = GetServerName();
+        public bool IsAZ { get; set; } = IsRunningAzure();
+
+        private static bool IsRunningAzure()
+        {
+            string? websiteSiteName = Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME");
+            string? websiteSku = Environment.GetEnvironmentVariable("WEBSITE_SKU");
+            string? websiteHostname = Environment.GetEnvironmentVariable("WEBSITE_HOSTNAME");
+
+            if (!string.IsNullOrEmpty(websiteSiteName))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static string GetServerName()
+        {
+            var procName = Process.GetCurrentProcess().ProcessName;
+            Log.Logger.Warning("[INT] Process: {name} ", procName);
+            return procName switch
+            {
+                "w3wp.exe" => "I6", // IIS6
+                "iisexpress.exe" => "IEX", // IISExpress
+                "dotnet.exe" => "Kestrel",
+                "Web.Data" => "http",
+                _ => "UNK",
+            };
+        }
     }
 }
